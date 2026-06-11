c66772836a9538167bd5fa66abbcf14a89add88b
alexko-cloud <msg.alexko@gmail.com>
2026-06-11 00:23:45 -0700

    Added style.css and updated footer.php

--- hero.php
+++ hero.php
@@ -3,62 +3,67 @@
 <head>
     <meta charset="UTF-8">
     <title></title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
-<?php
-
-$heroes = json_decode(file_get_contents("heroes.json"), true);
-$id = $_GET['id'] ?? '';
-
-if (!isset($heroes[$id])) {
-    header("Location: index.php");
-    exit;
-}
-
-$hero = $heroes[$id];
-
-include "includes/header.php";
-?>
-
-<section class="hero-banner">
-    <div class="hero-banner-image">
-        <img src="<?=$hero['image']?>" alt="<?=$hero['name']?>" width="300px" height="300px">
-    </div>
+<body>
+    <div class="bg-image"></div>
     
-    <div class="hero-banner-info">
-        <h1> <?= $hero['name']?> </h1>
-        <h2> <?= $hero['real_name']?> </h2>
-        <h3> <?= $hero['role']?> </h3>
-        <p> <?= $hero['bio']?> </p>
-    </div>
-</section>
+    <?php
+        $heroes = json_decode(file_get_contents("heroes.json"), true);
+        $id = $_GET['id'] ?? '';
 
-<section class="appearances">
-    <h2> Appears in: </h2>
+        if (!isset($heroes[$id])) {
+            header("Location: index.php");
+            exit;
+        }
 
-    <div class="comics">
-        <?php foreach($hero['comics'] as $comic): ?>
-            <a 
-                href="<?= $comic['link']?>" target="_blank"> 
-                <h3 class="comic-title"> <?= $comic['title'] ?> </h3>
-            </a>
-            
-            <p> <?= $comic['description'] ?></p>
+        $hero = $heroes[$id];
 
-        <?php endforeach; ?>
+        include "includes/header.php";
+    ?>
 
-        <?php foreach($hero['entertainment'] as $media): ?>
-            <a
-                href="<?= $media['link']?>" target="_blank">
-                <h3 class="media-title"> <?= $media['title'] ?> </h3>
-            </a>
+    <section class="hero-banner">
+        <div class="hero-banner-image">
+            <img src="<?=$hero['image']?>" alt="<?=$hero['name']?>" width="300px" height="300px">
+        </div>
+        
+        <div class="hero-banner-info">
+            <h1> <?= $hero['name']?> </h1>
+            <h2> <?= $hero['real_name']?> </h2>
+            <h3> <?= $hero['role']?> </h3>
+            <p> <?= $hero['bio']?> </p>
+        </div>
+    </section>
 
-            <p> <?= $media['description'] ?></p>
-        <?php endforeach; ?>
+    <section class="appearances">
+        <h2> Appears in: </h2>
+
+        <div class="comics">
+            <?php foreach($hero['comics'] as $comic): ?>
+                <a 
+                    href="<?= $comic['link']?>" target="_blank"> 
+                    <h3 class="comic-title"> <?= $comic['title'] ?> </h3>
+                </a>
+                
+                <p> <?= $comic['description'] ?></p>
+
+            <?php endforeach; ?>
+
+            <?php foreach($hero['entertainment'] as $media): ?>
+                <a
+                    href="<?= $media['link']?>" target="_blank">
+                    <h3 class="media-title"> <?= $media['title'] ?> </h3>
+                </a>
+
+                <p> <?= $media['description'] ?></p>
+            <?php endforeach; ?>
     </div>
 </section>
 
 <?php include "includes/footer.php"; ?>
+</body>
+
 
 </html>
 
--- index.php
+++ index.php
@@ -3,34 +3,39 @@
     <head> 
         <meta charset="UTF-8">
         <title> Rivals Codex </title>
-        <!-- insert css file here -->
+        <link rel="stylesheet" href="style.css">
     </head>
 
-    <?php
-        $heroes = json_decode(file_get_contents("heroes.json"), true);
-    ?>
+    <body>
+        <div class="bg-image"></div>
+        
+        <?php
+            $heroes = json_decode(file_get_contents("heroes.json"), true);
+        ?>
 
-    <div class="top-header"> 
-        <h1> Rivals Codex </h1>
-    </div>
-
-    <div class="hero-grid" id="heroGrid">
-        <?php foreach($heroes as $id => $hero): ?>
-
-        <a href="hero.php?id=<?= $id ?>" class="hero-card"
-        data-name="<?=$hero['name'] ?>"
-        data-role="<?= $hero['role'] ?>">
-
-        <div class="hero-image">
-            <img src="<?=$hero['image'] ?>" alt="<?=$hero['name']?>" width="300px" height="300px">
+        <div class="top-header"> 
+            <h1> Rivals Codex </h1>
         </div>
-        
-        <p> <?= $hero['name'] ?> </p>
-        
 
-        </a>
-        <?php endforeach; ?>
-    </div>
+        <div class="hero-grid" id="heroGrid">
+            <?php foreach($heroes as $id => $hero): ?>
 
-    <?php include "includes/footer.php" ?>
+            <a href="hero.php?id=<?= $id ?>" class="hero-card"
+            data-name="<?=$hero['name'] ?>"
+            data-role="<?= $hero['role'] ?>">
+
+            <div class="hero-image">
+                <img src="<?=$hero['image'] ?>" alt="<?=$hero['name']?>" width="300px" height="300px">
+            </div>
+            
+            <p> <?= $hero['name'] ?> </p>
+            
+
+            </a>
+            <?php endforeach; ?>
+        </div>
+
+        <?php include "includes/footer.php" ?>
+    </body>
+    
 </html>
